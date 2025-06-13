5c081ef25f27c934e6bc2c22920d0226b3c4d0a9
Diogenes Marques Risso <diogenes.risso@glok.com.br>
2025-06-13 18:01:13 -0300

    feat: wip login page

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from './login-page/login-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'client';
+  protected title = 'banana';
 }
